1fa106dc022c5eb48fa116fae30771fa8f313145
Jacob Johnson <jacob@Jacobs-MacBook-Pro.local>
2023-07-16 23:18:54 -0400

    created and trained custom classifier

---
+++ pneumonia_dataset.py
@@ -0,0 +1,36 @@
+import os
+import torch
+import pydicom
+from torchvision.transforms import transforms
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Define custom dataset class
+class PneumoniaDataset(Dataset):
+    def __init__(self, data_paths, labels, transforms=None):
+        self.data_paths = data_paths
+        self.labels = labels
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.data_paths)
+
+    def __getitem__(self, idx):
+        path = self.data_paths[idx]
+        # Read the DICOM file
+        dcm = pydicom.dcmread(path)
+        # Extract the pixel array from the DICOM file
+        img = dcm.pixel_array
+        img = Image.fromarray(img)
+
+        if self.transforms is not None:
+            # Apply the specified transforms to the image
+            img = self.transforms(img)
+
+        # Extract the label
+        label = self.labels[idx]
+        return img, label
